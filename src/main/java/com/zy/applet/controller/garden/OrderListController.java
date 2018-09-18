0496fa7a26912e886f1a5a7808df67e934296567
wq_zy <1573240324@qq.com>
2018-09-18 21:04:57 +0800

    ...

--- src/main/java/com/zy/applet/controller/garden/OrderListController.java
+++ src/main/java/com/zy/applet/controller/garden/OrderListController.java
@@ -3,8 +3,10 @@ package com.zy.applet.controller.garden;
 import com.zy.applet.pojo.Order;
 import com.zy.applet.service.OrderListService;
 import com.zy.applet.utils.ResponseMessageUtils;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -14,6 +16,8 @@ public class OrderListController {
     @Autowired
     private OrderListService orderListService;
 
+    @RequestMapping("shop")
+    @ApiOperation(value = "123")
     public ResponseMessageUtils selectOrderList(Integer isPay,String openid,Integer isShip) {
         List<Order> orderList = orderListService.selectOrderList(isPay, openid, isShip);
         return ResponseMessageUtils.ok(orderList);
