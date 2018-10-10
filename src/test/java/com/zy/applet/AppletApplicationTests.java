cdd67563cfc113006ca899a6d52b0976cac02340
zywaiting <1573240324@qq.com>
2018-10-11 01:05:14 +0800

    公众号

--- src/test/java/com/zy/applet/AppletApplicationTests.java
+++ src/test/java/com/zy/applet/AppletApplicationTests.java
@@ -1,16 +1,23 @@
 package com.zy.applet;
 
+import com.zy.applet.wxchat.config.WxMpProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableConfigurationProperties(WxMpProperties.class)
 public class AppletApplicationTests {
 
+	@Autowired
+	private WxMpProperties properties;
 	@Test
 	public void contextLoads() {
+		System.out.println(properties.getConfigs().get(0).getAppId());
 	}
 
 }
