005a1e2ca287e2bed652ac5f26ea44384eb316f3
zywaiting <1573240324@qq.com>
2019-01-19 00:31:30 +0800

    apollo

--- src/main/java/com/zy/applet/AppletApplication.java
+++ src/main/java/com/zy/applet/AppletApplication.java
@@ -1,8 +1,12 @@
 package com.zy.applet;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableApolloConfig
 @SpringBootApplication
 public class AppletApplication {
 
